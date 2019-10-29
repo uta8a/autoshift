6dced0171cf5e3e441b159ac97dabe239b726622
uta8a <noumodulity@gmail.com>
2019-10-29 13:23:21 +0900

    add: static

--- api/date.go
+++ api/date.go
@@ -1,10 +1,12 @@
 package handler
- 
+
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
- 
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>NO CONTENT</h1>")
+	currentTime := time.Now().Format(time.RFC850)
+	fmt.Fprintf(w, currentTime)
 }
